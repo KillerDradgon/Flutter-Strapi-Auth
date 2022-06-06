8ec8e5fcbfe5b588921d735475128af0f1f6ec95
Hassam Sohail <hassamsohail709@gmail.com>
2022-06-06 14:37:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter-Strapi-Auth
+ This Repository is a Simple example on how you can use strapi with flutter to login/signup .
---
+++ flutter/Frontend/lib/env/variables.dart
@@ -0,0 +1,7 @@
+//Strapi api url
+String api =
+    "https://ad86-188-70-52-168.in.ngrok.io/";
+
+
+
+/*Note If you are using Strapi locally it won't work instead use ngrok*/
